f635bfc7aa30061a52f9062d52a2db4fd67a985e
Qwuke <tristan.e.mortimer@gmail.com>
2018-10-22 20:29:42 -0400

    Add MC Escher Square Limit practice

---
+++ eschers-draw/cargo_home/registry/src/github.com-1ecc6299db9ec823/svg-0.5.11/src/node/text.rs
@@ -0,0 +1,46 @@
+use std::fmt;
+
+use node::{Node, Value};
+
+/// A text node.
+#[derive(Clone, Debug)]
+pub struct Text {
+    content: String,
+}
+
+impl Text {
+    /// Create a node.
+    #[inline]
+    pub fn new<T>(content: T) -> Self
+    where
+        T: Into<String>,
+    {
+        Text {
+            content: content.into(),
+        }
+    }
+}
+
+impl fmt::Display for Text {
+    #[inline]
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        self.content.fmt(formatter)
+    }
+}
+
+impl Node for Text {
+    #[inline]
+    fn append<T>(&mut self, _: T)
+    where
+        T: Node,
+    {
+    }
+
+    #[inline]
+    fn assign<T, U>(&mut self, _: T, _: U)
+    where
+        T: Into<String>,
+        U: Into<Value>,
+    {
+    }
+}
---
+++ eschers-draw/starter/README.md
@@ -0,0 +1,6 @@
+# `eschers`
+`Rust` starter code exploring functional geometry.
+
+## Prerequisites
+`Rust` version should be `1.26` or higher.
+
